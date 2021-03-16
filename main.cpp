2ae32e7cdfd10975e86fab884d02841337a4fe57
Jorge Ortiz <ortizjorge319@gmail.com>
2021-03-16 00:25:24 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <fstream>
+# include "menu.h"
+using namespace std;
+void heapsort(float b[],int n);
+void menu();
+int main() 
+{
+ menu();
+}
+
---
+++ menu.h
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <fstream>
+void heapsort(float b[],int n);
+void menu();
+using namespace std;
+void heapsort(float b[],int n)
+{
+  int a;
+  float temp,valor;
+  for(int i=n;i>0;i--)
+  {
+    for(int j=1;j<=i;j++)
+    {
+      valor=b[j];
+      a=j/2;
+      while(a>0&&b[a]<valor)
+    {
+      b[j]=b[a];
+      j=a;
+      a=a/2;
+    }
+
+    b[j]=valor;
+
+    }
+
+    temp=b[1];
+    b[1]=b[i];
+    b[i]=temp;
+  }
+}
+void menu(){
+   ofstream registro;
+  registro.open("Heapsort.txt",ios::app);
+  registro<<endl;
+  registro<<"SIN ORDENAR"<<endl;
+
+  float numerosuno[50];
+  int numeros;
+  cout<<"Ingrese el numero de elementos"<<endl;
+  cin>>numeros;
+
+  for(int i=1;i<=numeros;i++)
+  {
+    cout<<"Numero ["<<i<<"]"<<endl;
+    cin>>numerosuno[i];
+  }
+  cout<<"arreglo de elementos originales"<<endl;
+  registro<<"arreglo de elementos originales"<<endl;
+
+  for(int i=1;i<=numeros;i++)
+  {
+    cout<<"|"<<numerosuno[i];
+    registro<<"|"<<numerosuno[i]<<"|";
+  }
+  heapsort(numerosuno, numeros);
+  registro<<endl;
+  cout<<endl;
+
+  registro<<"HEAPSORT"<<endl;
+  cout<<"Ordenamiento"<<endl;
+  cout<<"arreglo Heapsort"<<endl;
+
+  for(int i=1;i<=numeros;i++)
+  {
+    cout<<"|"<<numerosuno[i];
+    registro<<"|"<<numerosuno[i]<<"|";
+  }
+
+}
